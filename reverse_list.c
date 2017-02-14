d2adfe2ba04996e86ff6cbb8c46b42ee1e7abdae
zkpig123 <1052580590@qq.com>
2017-02-14 14:44:57 +0800

    add reverse_list.c

---
+++ reverse_list.c
@@ -0,0 +1,25 @@
+//1.reverse unidirectional link list, given pointer to current
+//list, pointer to function that get next item from current 
+//item, pointer to function that set next item of current
+//item
+//2.use reverse_list, don't use _reverse_list directly
+#include <stdlib.h>
+
+void *_reverse_list (void *previous_item, void *current_item, void *(*get_next_item)(void *item), int (*set_next_item)(void *item, void *desired_next_item))
+{
+	void *next_item;
+	if (current_item == NULL) return NULL;
+	next_item = get_next_item(current_item);
+	set_next_item (current_item, previous_item);
+	if (next_item == NULL) return current_item;
+	else return _reverse_list(current_item, next_item, get_next_item, set_next_item);
+}
+
+void *reverse_list (void *list, void *(*get_next_item)(void *item), int (*set_next_item)(void *item, void *desired_next_item))
+{
+	void *next;
+	if (list == NULL) return NULL;
+	next = get_next_item (list);
+	if (next == NULL) return list;
+	else return _reverse_list (list, next, get_next_item, set_next_item);
+}
