483f4ebd0e936a32f336749c41c77cc2a80dbe21
zkpig123 <1052580590@qq.com>
2017-02-14 16:15:00 +0800

    modify binary_tree.c reverse_lsit.c

--- binary_tree.c
+++ binary_tree.c
@@ -1,6 +1,9 @@
+//1.binary tree corresponding func
+
 #include <stdlib.h>
 
-void *_binary_tree_lookup_recursive (void *node, void *target_value, void *(*get_left_node)(void *node), void *(*get_right_node)(void *node), void *get_node_value(void *node), int (*compare_node_value)(void *node_value, void *target_value))
+//invoked by recursive lookup function
+static void *_lookup_binary_tree_recursive (void *node, void *target_value, void *(*get_left_node)(void *node), void *(*get_right_node)(void *node), void *get_node_value(void *node), int (*compare_node_value)(void *node_value, void *target_value))
 {
 	void *left_node, *right_node;
 	void *node_value;
@@ -9,29 +12,36 @@ void *_binary_tree_lookup_recursive (void *node, void *target_value, void *(*get
 	node_value = get_node_value(node);
 	if((ret = compare_node_value (node_value, target_value)) == 0) return node;
 	else if(ret < 0){
-		if ((left_node = get_left_node(node)) != NULL) return _binary_tree_lookup_recursive(left_node, target_value, get_left_node, get_right_node, get_node_value, compare_node_value);
+		if ((left_node = get_left_node(node)) != NULL) return _lookup_binary_tree_recursive(left_node, target_value, get_left_node, get_right_node, get_node_value, compare_node_value);
 		else return NULL;
 	}
 	else{
-		if ((right_node = get_right_node(node)) != NULL) return _binary_tree_lookup_recursive(right_node, target_value, get_left_node, get_right_node, get_node_value, compare_node_value);
+		if ((right_node = get_right_node(node)) != NULL) return _lookup_binary_tree_recursive(right_node, target_value, get_left_node, get_right_node, get_node_value, compare_node_value);
 		else return NULL;
 
 	}
 }
 
-void *binary_tree_lookup_recursive (void *node, void *target_value, void *(*get_left_node)(void *node), void *(*get_right_node)(void *node), void *get_node_value(void *node), int (*compare_node_value)(void *node_value, void *target_value))
+//recursive version of lookup(binary tree is in order, 
+//given pointer to function to get pointer of left and
+//right node of node, pointer to function to get value of 
+//node, pointer to function to compare node value
+void *lookup_binary_tree_recursive (void *node, void *target_value, void *(*get_left_node)(void *node), void *(*get_right_node)(void *node), void *get_node_value(void *node), int (*compare_node_value)(void *node_value, void *target_value))
 {
 	if (node == NULL || target_value == NULL || get_left_node == NULL || get_right_node == NULL || get_node_value == NULL || compare_node_value == NULL) return NULL;
-	return _binary_tree_lookup_recursive (node, target_value, get_left_node, get_right_node, get_node_value, compare_node_value);
+	return _lookup_binary_tree_recursive (node, target_value, get_left_node, get_right_node, get_node_value, compare_node_value);
 }
 
-void *binary_tree_lookup_loop (void *node, void *target_value, void *(*get_left_node)(void *node), void *(*get_right_node)(void *node), void *get_node_value(void *node), int (*compare_node_value)(void *node_value, void *target_value))
+//loop version of lookup(binary tree is in order), 
+//args are the same as recursive version
+void *lookup_binary_tree_loop (void *start_node, void *target_value, void *(*get_left_node)(void *node), void *(*get_right_node)(void *node), void *get_node_value(void *node), int (*compare_node_value)(void *node_value, void *target_value))
 {
-	void *left_node, *right_node;
+	void *node;
 	void *node_value;
 	int ret;
 
-	if (node == NULL || target_value == NULL || get_left_node == NULL || get_right_node == NULL || get_node_value == NULL || compare_node_value == NULL) return NULL;
+	if (start_node == NULL || target_value == NULL || get_left_node == NULL || get_right_node == NULL || get_node_value == NULL || compare_node_value == NULL) return NULL;
+	node = start_node;
 	while (1){
 
 		node_value = get_node_value(node);
@@ -47,4 +57,18 @@ void *binary_tree_lookup_loop (void *node, void *target_value, void *(*get_left_
 		}
 	}
 	return NULL; //should never get here
+}
+
+static void _traverse_binary_tree_recursive (void *node, void *(*get_left_node)(void *node), void *(*get_right_node)(void *node), void traverse_func(void *node))
+{
+	void *left_node, *right_node;
+	if ((left_node = get_left_node(node)) != NULL) _traverse_binary_tree_recursive (left_node, get_left_node, get_right_node, traverse_func);
+	(*traverse_func)(node);
+	if ((right_node = get_right_node(node)) != NULL) _traverse_binary_tree_recursive (right_node, get_right_node, get_right_node, traverse_func);
+}
+
+void traverse_binary_tree_recursive (void *root, void *(*get_left_node)(void *node), void *(*get_right_node)(void *node), void traverse_func(void *node))
+{
+	if (node == NULL || get_left_node == NULL || get_right_node == NULL || traverse_func == NULL) return NULL;
+	_traverse_binary_tree_recursive(root, get_left_node, get_right_node, traverse_func);
 }
--- reverse_list.c
+++ reverse_list.c
@@ -1,25 +1,46 @@
-//1.reverse unidirectional link list using both recursive method and loop method, given pointer to current
-//list, pointer to function that get next item from current 
-//item, pointer to function that set next item of current
-//item
-//2.use reverse_list_recursive or rever_list_loop, don't use _reverse_list directly
+//link list corresponding function
 #include <stdlib.h>
 
-void *_reverse_list (void *previous_item, void *current_item, void *(*get_next_item)(void *item), int (*set_next_item)(void *item, void *desired_next_item))
+//invoked by reverse function
+static void *_reverse_list_recursive (void *previous_item, void *current_item, void *(*get_next_item)(void *item), int (*set_next_item)(void *item, void *desired_next_item))
 {
 	void *next_item;
 	if (current_item == NULL) return NULL;
 	next_item = get_next_item(current_item);
 	set_next_item (current_item, previous_item);
 	if (next_item == NULL) return current_item;
-	else return _reverse_list(current_item, next_item, get_next_item, set_next_item);
+	else return _reverse_list_recursive(current_item, next_item, get_next_item, set_next_item);
 }
 
+//reverse unidirectional link list using recursive method,
+//given pointer to current list, pointer to function that
+//get next item from current item, pointer to function
+//that set next item of current item, return pointer to 
+//first item
 void *reverse_list_recursive (void *list, void *(*get_next_item)(void *item), int (*set_next_item)(void *item, void *desired_next_item))
 {
 	void *next;
+
+	if (list == NULL || get_next_item == NULL || set_next_item == NULL) return NULL;
 	if (list == NULL) return NULL;
 	next = get_next_item (list);
 	if (next == NULL) return list;
-	else return _reverse_list (list, next, get_next_item, set_next_item);
+	else return _reverse_list_recursive (list, next, get_next_item, set_next_item);
+}
+
+//reverse link list using loop, args are the same as recursive
+//version, return pointer to first item
+void *reverse_list_loop (void *list, void *(*get_next_item)(void *item), int (*set_next_item)(void *item, void *desired_next_item))
+{
+	void *next_item, *current_item, *previous_item;
+	if (list == NULL || get_next_item == NULL || set_next_item == NULL) return NULL;
+	previous_item = NULL;
+	current_item = list;
+	while (1){
+
+		next_item = get_next_item(current_item);
+		set_next_item (current_item, previous_item);
+		if (next_item == NULL) return current_item;
+		else current_item = next_item;
+	}
 }
