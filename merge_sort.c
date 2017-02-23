7b39128b4d4be0819a705c270172140f9df3fef9
zkpig123 <zkpig123@yahoo.com>
2017-02-23 17:28:07 +0800

    merge_sort.c

--- merge_sort.c
+++ merge_sort.c
@@ -2,15 +2,15 @@
 #include <string.h>
 #include <limits.h>
 
-int _merge_sort_incremental (int cards[], size_t card_num)
+int _merge_sort_guard_incremental_guard (int cards[], size_t card_num)
 {
 	size_t left_card_num;
 	size_t right_card_num;
 	size_t mid = card_num / 2;
 	left_card_num = mid;
 	right_card_num = card_num - left_card_num;
-	_merge_sort_incremental(cards, left_card_num);
-	_merge_sort_incremental(cards + mid, right_card_num);
+	_merge_sort_guard_incremental_guard(cards, left_card_num);
+	_merge_sort_guard_incremental_guard(cards + mid, right_card_num);
 
 	int *left_cards, *right_cards;
 	if ((left_cards = malloc(left_card_num + 1)) == NULL || (right_cards = malloc(right_card_num + 1)) == NULL) return 1;
@@ -29,15 +29,15 @@ int _merge_sort_incremental (int cards[], size_t card_num)
 	free(right_cards);
 }
 
-int _merge_sort_decremental (int cards[], size_t card_num)
+int _merge_sort_guard_decremental_guard (int cards[], size_t card_num)
 {
 	size_t left_card_num;
 	size_t right_card_num;
 	size_t mid = card_num / 2;
 	left_card_num = mid;
 	right_card_num = card_num - left_card_num;
-	_merge_sort_incremental(cards, left_card_num);
-	_merge_sort_incremental(cards + mid, right_card_num);
+	_merge_sort_guard_incremental_guard(cards, left_card_num);
+	_merge_sort_guard_incremental_guard(cards + mid, right_card_num);
 
 	int *left_cards, *right_cards;
 	if ((left_cards = malloc(left_card_num + 1)) == NULL || (right_cards = malloc(right_card_num + 1)) == NULL) return 1;
@@ -56,11 +56,11 @@ int _merge_sort_decremental (int cards[], size_t card_num)
 	free(right_cards);
 }
 
-int merge_sort (int cards[], size_t card_num, int order)
+int merge_sort_guard (int cards[], size_t card_num, int order)
 {
 	if (cards == NULL || card_num <= 0 || order == 0) return 1;
 	else if (card_num == 1) return 0;
-	if (order > 0) _merge_sort_incremental(cards, card_num);
-	else _merge_sort_decremental(cards, card_num);
+	if (order > 0) _merge_sort_guard_incremental_guard(cards, card_num);
+	else _merge_sort_guard_decremental_guard(cards, card_num);
 	return 0;
 }
